5c3f89e8421c13cdf097a2e259e45ef1ce49ea7d
zblock135 <zblock135@gmail.com>
2014-08-25 17:36:23 -0400

    Revert "Adding all App Files"
    This reverts commit 7f87acfb10a5974c0364fcb9132b74ac03e60e06.

--- gen/com/EPED/pramodbopit/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.EPED.pramodbopit;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/com/EPED/pramodbopit/OnShakeListener.java
+++
@@ -1,5 +0,0 @@
-package com.EPED.pramodbopit;
-
-public interface OnShakeListener {
-	public void onShake();
-}
